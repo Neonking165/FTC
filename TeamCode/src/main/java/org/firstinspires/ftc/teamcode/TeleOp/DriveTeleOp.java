45184ed150447bf6f324d817666ebaafdc54a495
GraggleTownHousePurchaser <alexmatthews256@gmail.com>
2024-08-29 10:26:27 +1000

    Added Frequency measure

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DriveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DriveTeleOp.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Drive.RemoteDrive;
 import org.firstinspires.ftc.teamcode.Subsystems.ServoTest;
@@ -15,6 +16,12 @@ public class DriveTeleOp extends OpMode {
 
     private ServoTest testServo;
 
+    private ElapsedTime time;
+
+    private double startTime;
+
+    private double endTime;
+
     public void init() {
         tankDrive = new RemoteDrive(hardwareMap);
         tankDrive.Drive(0,0);
@@ -22,6 +29,10 @@ public class DriveTeleOp extends OpMode {
         testServo = new ServoTest(hardwareMap);
         telemetry.addData("Status", "Initialised");
         telemetry.update();
+        time = new ElapsedTime();
+
+        startTime = 0;
+        endTime = 0;
     }
 
     /*
@@ -38,7 +49,12 @@ public class DriveTeleOp extends OpMode {
 
     @Override
     public void loop() {
+        
+        endTime =  time.milliseconds();
+        double frequency = 1 /  ((endTime - startTime) / 1000);
+        telemetry.addData("Frequency", frequency + "Hz");
 
+        startTime = time.milliseconds();
         double x  =  gamepad1.right_stick_x;
         double y = -gamepad1.left_stick_y;
 
@@ -50,10 +66,11 @@ public class DriveTeleOp extends OpMode {
             testServo.SetPosition(0);
         }
 
+
+
         telemetry.addData("Status", "Running");
         telemetry.addData("Drive Power", "Left: " + tankDrive.GetLeftVelocity() + "Right: " + tankDrive.GetRightVelocity());
         telemetry.addData("Servo Position", testServo.GetPosition());
-
         telemetry.update();
     }
 
